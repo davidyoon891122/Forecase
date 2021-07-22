14768e52cd4bbe32363048dfdf9f11378750d973
davidyoon <qqpo12@naver.com>
2021-07-22 23:21:04 +0900

    add TableViewDataSource and set section number and section cell

--- Forecase/ViewController.swift
+++ Forecase/ViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var listTableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,3 +20,30 @@ class ViewController: UIViewController {
 
 }
 
+
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 0
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: SummaryTableViewCell.identifier, for: indexPath)
+            return cell
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: ForecaseTableViewCell.identifier, for:indexPath)
+        return cell
+    }
+}
